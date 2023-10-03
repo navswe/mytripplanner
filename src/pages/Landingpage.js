744744c0c745c9e8b8f2638c4f6599a0d4ee38fb
An Nguyen <navdevengr@gmail.com>
2023-10-02 19:28:48 -0500

    10-2 styling and cleanup

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -2,23 +2,23 @@ import React from "react";
 import Landingpageform from "../components/Authentication/Landingpageform";
 import { Container } from "@mui/material";
 import "./Landingpage.css";
-import { useEffect } from "react";
-import WAVES from "vanta/src/vanta.waves";
+// import { useEffect } from "react";
+// import WAVES from "vanta/src/vanta.waves";
 
 const Landingpage = () => {
-  useEffect(() => {
-    WAVES({
-      el: "#your-element-selector",
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      scale: 1.0,
-      scaleMobile: 1.0,
-      color: 0x3f88,
-    });
-  });
+  // useEffect(() => {
+  //   WAVES({
+  //     el: "#vanta",
+  //     mouseControls: true,
+  //     touchControls: true,
+  //     gyroControls: false,
+  //     minHeight: 200.0,
+  //     minWidth: 200.0,
+  //     scale: 1.0,
+  //     scaleMobile: 1.0,
+  //     color: 0x3f88,
+  //   });
+  // });
   return (
     <Container className="container" id="vanta">
       <Landingpageform />
